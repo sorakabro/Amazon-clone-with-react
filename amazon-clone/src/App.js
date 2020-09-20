bacb432cf96ecc3219dfa88b69178bcfe9be0892
Benjamin Oldén <berntsson90@hotmail.com>
2020-09-20 13:20:37 +0200

    intial commit

---
+++ amazon-clone/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+   <h1>Lets build an Amazon clone.</h1>
+   <h2>Hello World</h2>
+
+   {/* We Need React-Router*/}
+   
+    </div>
+  );
+}
+
+export default App;
