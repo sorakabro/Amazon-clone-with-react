45baa5d8b74a12de5f8770df971088321fd87cf1
Benjamin Oldén <berntsson90@hotmail.com>
2020-09-20 13:54:27 +0200

    setting up react header

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-   <h1>Lets build an Amazon clone.</h1>
-   <h2>Hello World</h2>
-
-   {/* We Need React-Router*/}
-   
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <h1>Checkout</h1>
+          </Route>
+          <Router path="/login">
+            <h1>Login page</h1>
+          </Router>
+          {/* This is the Default route*/}
+          <Route path="/">
+            <h1>Home Page!!!!</h1>
+          </Route>
+        </Switch>
+     </div>
+    </Router>
   );
 }
+
+{/* We Need React-Router*/}
 
 export default App;
---
+++ amazon-clone/src/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+    return 
+        <div>
+            
+    {/* Logo on the left -> img */}
+    {/*  Search box*/}
+    {/* 3 Links*/}
+    {/* Basket icon with number*/}
+        </div>;  
+}
+
+export default Header
