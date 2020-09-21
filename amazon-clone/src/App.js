6e8d68ff6f2a56b21cdf5a26da8efd74b984f7d1
Benjamin Oldén <berntsson90@hotmail.com>
2020-09-21 10:39:37 +0200

    Text input Header

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Header from "./Header";
+
 
 function App() {
   return (
@@ -15,6 +17,7 @@ function App() {
           </Route>
           {/* This is the Default route*/}
           <Route path="/">
+            <Header />
             <h1>Home Page!!!!</h1>
           </Route>
         </Switch>
--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -1,12 +1,17 @@
 import React from "react";
+import "./Header.css";
+import {Link} from "react-router-dom";
 
 function Header() {
     return <nav className="header">
         
-            
+        <Link to="/">  
     {/* Logo on the left -> img */}
-    <img className ="header_logo" src="./images/amazon-logo.png" />
+    <img className ="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="" />
+    </Link>  
     {/*  Search box*/}
+
+    <input type="text" className="header__searchInput"/>
     {/* 3 Links*/}
     {/* Basket icon with number*/}
     </nav>  
