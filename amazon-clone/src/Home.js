34b42a5ef126bdadcf573d57231c5d146acf084d
Benjamin Oldén <berntsson90@hotmail.com>
2020-09-21 14:47:26 +0200

    product items

--- amazon-clone/src/Home.js
+++ amazon-clone/src/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Product from "./Product";
 import "./Home.css";
 
 function Home() {
@@ -6,6 +7,17 @@ function Home() {
         <div className="home">
             <img className="home__image"    
             src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/gateway/placement/launch/TheBoysS2/THBY_2020_GWBleedingHero_FT_COVIDUPDATE_XSite_3000x1200_PV_en-GB._CB406828669_.jpg" alt="" />
+        
+
+        {/* Product id, title, price, rating, image*/}
+        <Product 
+        id="12321" 
+        title="The Lean Startup: How Constant Innovation Creates companys!"
+        price= {11.96}
+        rating= {5}
+        image="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg" 
+        />
+        {/* Product */}
         </div>
     );
 }
---
+++ amazon-clone/src/Product.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import "./Product.css";
+
+function Product({id, title, image, price, rating}) {
+    return ( 
+    
+    <div className="product">
+            <p>{title}</p>
+            <p className="product__price">
+                <small>$</small>
+                <strong>{price}</strong>
+            </p>
+            <div className="product__rating">
+                {
+                    Array(rating) 
+                    .fill()
+                    .map((_) => (
+                        <p>★</p>
+                    ))}
+            </div>
+        </div>
+        
+    );
+    
+}
+
+export default Product
